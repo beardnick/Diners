4e16b4ca0ff07cc087c5323b80bdd1934b43b303
Nick <1685437606@qq.com>
2018-02-23 12:46:41 +0800

    添加了模糊搜索功能，但是Recyclerview不能显示出来

--- app/src/main/java/com/example/asus/diners/Utils/DataBaseUtil.java
+++ app/src/main/java/com/example/asus/diners/Utils/DataBaseUtil.java
@@ -4,9 +4,19 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 
+import com.example.asus.diners.Model.Dish;
+import com.example.asus.diners.View.DishAdapter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import cn.bmob.v3.BmobQuery;
 import cn.bmob.v3.datatype.BmobFile;
 import cn.bmob.v3.exception.BmobException;
 import cn.bmob.v3.listener.DownloadFileListener;
+import cn.bmob.v3.listener.FindListener;
 
 /**
  * Created by asus on 2018/2/12.
@@ -43,4 +53,33 @@ public class DataBaseUtil{
         return image;
     }
 
+    public static ArrayList<Dish> searchDish(String queryString){
+        BmobQuery<Dish> query = new BmobQuery<>();
+        final char[] queryChars = queryString.toCharArray();
+        for(char x : queryChars)Log.v( TAG , String.valueOf(x));
+        final ArrayList<Dish> result = new ArrayList<>();
+        query.findObjects(new FindListener<Dish>() {
+            @Override
+            public void done(List<Dish> list, BmobException e) {
+                if(e == null){
+                    for(Dish x :list){
+                        boolean ok = false;
+                        for(char y : queryChars){
+                            if(x.getName().contains(String.valueOf(y))){
+                                ok = true;
+                                break;
+                            }
+                        }
+                        if(ok){
+                            result.add(x);
+                        }
+                    }
+                    Log.v(TAG , "查询成功，共：" + result.size() + " 条数据");
+                }else{
+                    Log.v(TAG , "查询失败" + e.getMessage());
+                }
+            }
+        });
+        return result;
+    }
 }
